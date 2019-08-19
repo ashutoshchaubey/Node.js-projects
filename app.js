3b90cc7f121d0c29583822c069a1b88079bc1123
Ashutosh Kumar <35742041+Akcodes@users.noreply.github.com>
2019-08-19 16:30:29 +0530

    moved the app.js code to routes.js

--- app.js
+++ app.js
@@ -1,43 +1,11 @@
 const http = require("http");
-const fs = require("fs");
+
+const routes = require("./routes");
+// here we added a './' because the routes is not a global path
 
 // this function is called by nodejs whenever a request reaches our server
-const server = http.createServer((req, res) => {
-  // console.log(req.url, req.method, req.headers);
-  // process.exit();
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter message</title></head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>send</button></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", chunk => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err)=>{
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end;
-      }); 
-    });
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My first page</title></head>");
-  res.write("<body><h1>Hello from my node.js!</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+
+const server = http.createServer(routes);
+// here we are using routes as a handler
 
 server.listen(3000);
