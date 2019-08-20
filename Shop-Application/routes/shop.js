df33f10a1e4911816c21ba1b2e8bc3bcb4b67813
Ashutosh Kumar <35742041+Akcodes@users.noreply.github.com>
2019-08-20 20:14:18 +0530

    Shop_Application
    Added initial features to shop application

---
+++ Shop-Application/routes/shop.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+const express = require("express");
+
+const rootDir = require("../util/path");
+const adminData = require("./admin");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  // console.log("shop.js", adminData.products);
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  res.render("shop");
+});
+
+module.exports = router;
